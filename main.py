7c29a8be4849e7b91ed71767dea4ed5f8c58adf3
iiEthan <ethangibbs2@icloud.com>
2021-06-03 21:26:24 -0500

    ping, requirements

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ bot = commands.Bot(command_prefix = "e!", case_insensitive = True, help_command 
 async def on_ready():
 	print("Logged in as {0.user}".format(bot))
 
+# Gets token to authenticate bot
 with open("token.json") as bot_token:
 	data = json.load(bot_token)
 	token = data["token"]
+
+@bot.command()
+async def ping(ctx):
+   	await ctx.send(f':ping_pong: Pong! {round(bot.latency * 1000)}ms')
+
 
 bot.run(token)
